151ed68ca6ac7f9d2e9839d3e1fe7819986dadfa
ERIK JOEL GAIBOR GUACHAMIN <erik.gaibor@epn.edu.ec>
2026-02-26 04:51:55 -0500

    Fix: Fecha UTC

--- src/main/java/com/pico_placa_api/PicoPlacaApiApplication.java
+++ src/main/java/com/pico_placa_api/PicoPlacaApiApplication.java
@@ -1,11 +1,20 @@
 package com.pico_placa_api;
 
+import jakarta.annotation.PostConstruct; // Importante para que @PostConstruct funcione
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone; // Importante para manejar la zona horaria
+
 @SpringBootApplication
 public class PicoPlacaApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PicoPlacaApiApplication.class, args);
+    }
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("America/Guayaquil"));
+        System.out.println("ZONA HORARIA CONFIGURADA: " + TimeZone.getDefault().getID());
     }
 }
